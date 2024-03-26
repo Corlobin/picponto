6f90d5b444f570670c22d0e339a074a105442fa2
Cesar Quadros <cesar.quadros@picpay.com>
2024-03-26 10:21:42 -0300

    open adp

--- chrome-extension/assets/js/background.js
+++ chrome-extension/assets/js/background.js
@@ -6,6 +6,7 @@ chrome.alarms.onAlarm.addListener((alarm) => {
             const now = new Date();
             const startTime = new Date();
             const endTime = new Date();
+            var show = false;
             
             const [startHours, startMinutes] = result.startTime.split(':');
             const [endHours, endMinutes] = result.endTime.split(':');
@@ -27,21 +28,27 @@ chrome.alarms.onAlarm.addListener((alarm) => {
 
             if (now < startTime && new Date(now.getTime() + 60000) >= startTime) {
                 notificationOptions.message = 'A hora de bater o ponto para a entrada esta chegando.';
+                show = true;
             } else if (now >= startTime && now <= new Date(startTime.getTime() + 60000)) {
                 notificationOptions.message = 'Lembre-se de bater o ponto de entrada.';
+                show = true;
             } else if (now < endTime && new Date(now.getTime() + 60000) > endTime) {
                 notificationOptions.message = 'Lembre-se de bater o ponto da saída.';
+                show = true;
             }
 
-            chrome.notifications.create(null, notificationOptions, function(notificationId) {
-                // Adiciona um evento de clique à notificação após a criação
-                chrome.notifications.onClicked.addListener(function(clickedNotificationId) {
-                    if (clickedNotificationId === notificationId) {
-                        // Abre a URL desejada em uma nova aba
-                        chrome.tabs.create({ url: "https://adp.picpay.com" });
-                    }
+            if(show){
+                chrome.notifications.create(null, notificationOptions, function(notificationId) {
+                    // Adiciona um evento de clique à notificação após a criação
+                    chrome.notifications.onClicked.addListener(function(clickedNotificationId) {
+                        if (clickedNotificationId === notificationId) {
+                            // Abre a URL desejada em uma nova aba
+                            chrome.tabs.create({ url: "https://adp.picpay.com" });
+                        }
+                    });
                 });
-            });
+            }
+
         });
     }
 });
